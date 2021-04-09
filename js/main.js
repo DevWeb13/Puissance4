935098aafb24563f4485da0dacd3c9314e986b59
DevWeb13 <rambo13010@hotmail.fr>
2021-04-09 18:08:23 +0200

    Mise en place de la grille et du changement de couleur à chaque tour en JS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log("test");
+// Define HTML elements
+// Set up player & color
+let currentColor = "R";
+const grid = [
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
+];
+
+//Change color every round
+const toggleColor = () => {
+    currentColor = currentColor === 'R' ? 'Y' : 'R';//Si currentcolor vaut 'Rouge' alors on le passe en 'Yellow' sinon il repasse a 'Rouge'.
+}
+
+// Display grid
+// Add event listeners
+// // Get input from keyboard to play
+// // // Put tokrn in grid
+// Check win
+// Opt: Check draw
+// Opt: Add AI
